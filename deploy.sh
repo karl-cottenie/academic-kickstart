018856acc85319d055f39470a5f8b88e333677db
Karl Cottenie <karl.cottenie@gmail.com>
2019-03-08 11:17:52 -0500

    2019-03-08 deploy relative links

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # run ./deploy.sh "Your optional commit message" to send changes to karl-cottenie.github.io.
 
 # sync the Public folder to the karl-cotttenie.github.io folder
-rsync -aE -delete "/Users/karlcottenie/Google Drive/lab/website/academic-kickstart/public/" "/Users/karlcottenie/Google Drive/lab/website/karl-cottenie.github.io/"
+rsync -aE -delete "./public/" "../karl-cottenie.github.io/"
 
 # Go To karl-cottenie.github.io folder
-cd /Users/karlcottenie/Google\ Drive/lab/website/karl-cottenie.github.io
+cd ../karl-cottenie.github.io
 
 # Add changes to git.
 git add .
@@ -24,4 +24,4 @@ git commit -m "$msg"
 git push origin master
 
 # Come Back up to the Project Root
-cd /Users/karlcottenie/Google\ Drive/lab/website/academic-kickstart
+cd ../academic-kickstart
